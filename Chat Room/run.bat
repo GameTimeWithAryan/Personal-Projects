496c343e32c9081973e3d91206d91b3487a2eb90
GameTime With Aryan <gametimewitharyan@gmail.com>
2023-03-09 14:00:20 +0530

    Removed chat room project

--- Chat Room/run.bat
+++
@@ -1,3 +0,0 @@
-start cmd /c "mode con: cols=50 lines=10 & python server.py"
-start cmd /c "mode con: cols=50 lines=10 & python client.py GWA"
-start cmd /c "mode con: cols=50 lines=10 & python client.py GameTimeWA"
