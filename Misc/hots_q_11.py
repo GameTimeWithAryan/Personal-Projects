a5a30ad0660b11c7f4ec4a6a5954179793dc603d
GameTime With Aryan <gametimewitharyan@gmail.com>
2023-05-01 18:38:39 +0530

    Added Hots Q-11

---
+++ Misc/hots_q_11.py
@@ -0,0 +1,40 @@
+from fractions import Fraction
+
+"""
+General form = ax^2 + bx + c
+Polynomial p(x) = a * (x-(p+1)/p) * (x-p/(p-1))
+
+Find (a+b+c)^2
+"""
+
+
+def get_coef(a, p):
+    zero1 = Fraction((p + 1), p)
+    zero2 = Fraction(p, (p - 1))
+
+    b = a * -1 * (zero1 + zero2)
+    c = a * zero1 * zero2
+    return a, b, c
+
+
+def get_answer_from_coef(a, b, c):
+    return (a + b + c) ** 2
+
+
+def get_expected_answer(a, p):
+    return Fraction(a, (p * (p - 1)))
+
+
+def main():
+    for a in range(1, 11):
+        for p in range(2, 11):
+            answer_from_coef = get_answer_from_coef(*get_coef(a, p))
+            expected_answer = get_expected_answer(a, p)
+
+            if answer_from_coef == expected_answer:
+                print(f"{a=}, {p=}")
+                print(f"Coef: {answer_from_coef} | Expected: {expected_answer}")
+
+
+if __name__ == "__main__":
+    main()
